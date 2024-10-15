3e9753fb61538f1a66c0a21f70483d309502f9ac
Isaiah Mann <isaiahbmann@gmail.com>
2024-10-14 19:11:59 -0700

    Add clerk for auth

--- src/app/api/moods/route.ts
+++ src/app/api/moods/route.ts
@@ -2,16 +2,20 @@ export const dynamic = 'force-dynamic'
 
 import { neon } from '@neondatabase/serverless'
 import { encryptData, decryptData } from '@/lib/encrypt'
-
-let user_id = 'test_user'
+import { currentUser } from '@clerk/nextjs/server'
 
 export async function GET(request: Request) {
   if (!process.env.DATABASE_URL || !process.env.ENCRYPTION_KEY) {
     return new Response('Missing required env variable(s)', { status: 500 })
   }
 
+  const user = await currentUser()
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+
   const sql = neon(process.env.DATABASE_URL)
-  const data = await sql`SELECT * FROM moods where user_id = ${user_id}`
+  const data = await sql`SELECT * FROM moods where user_id = ${user.id}`
   const decryptedData = data.map((row: any) => ({
     ...row,
     mood: decryptData(row.mood, process.env.ENCRYPTION_KEY || '')
@@ -24,9 +28,14 @@ export async function POST(request: Request) {
     return new Response('Missing required env variable(s)', { status: 500 })
   }
 
+  const user = await currentUser()
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+
   const sql = neon(process.env.DATABASE_URL)
   const { mood } = await request.json()
   const encryptedMood = encryptData(mood, process.env.ENCRYPTION_KEY)
-  const data = await sql`INSERT INTO moods (user_id, mood, timestamp) VALUES (${user_id}, ${encryptedMood}, ${new Date()})`
+  const data = await sql`INSERT INTO moods (user_id, mood, timestamp) VALUES (${user.id}, ${encryptedMood}, ${new Date()})`
   return new Response(JSON.stringify(data), { status: 201 })
 }
